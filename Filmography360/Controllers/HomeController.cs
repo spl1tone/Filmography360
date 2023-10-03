62dbc873e4718df9a4c8587fd385e5666c0c9113
spl1tOne <levonvlad144@gmail.com>
2023-10-03 19:42:20 +0300

    some changes

--- Filmography360/Controllers/HomeController.cs
+++ Filmography360/Controllers/HomeController.cs
@@ -16,23 +16,20 @@ public class HomeController : Controller
 
     public IActionResult FilmInfoDark (int id)
     {
-        // Отримайте дані про фільм зі списку FilmList за ідентифікатором.
         var film = Filmography360.FakerInfo.FakerInfo.FilmList.FirstOrDefault(f => f.Id == id);
 
         ViewBag.Id = film.Id;
 
-        // Передайте дані про фільм в перегляд для відображення.
         return View("filmInfoDark");
     }
 
+    [Route("Home/ActorInfoDark/{id}")]
     public IActionResult ActorInfoDark (int id)
     {
-        // Отримайте дані про фільм зі списку FilmList за ідентифікатором.
         var actor = Filmography360.FakerInfo.FakerInfo.ActorList.FirstOrDefault(a => a.Id == id);
 
         ViewBag.ActorId = actor.Id;
 
-        // Передайте дані про фільм в перегляд для відображення.
         return View("actorInfoDark");
     }
 
